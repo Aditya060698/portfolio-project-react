62d23afb25e7bc9e9a42e997ffa7fbd9c51c0380
Aditya060698 <mishraditya06@gmail.com>
2024-05-07 11:36:19 +0530

    latest changes

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,26 +1,59 @@
 import Sidebar from "../sidebar/Sidebar";
 import "./navbar.scss";
 import { motion } from "framer-motion";
-
 const Navbar = () => {
   return (
     <div className="navbar">
       {/* Sidebar */}
-      <Sidebar/>
+      <Sidebar />
       <div className="wrapper">
-        <motion.span
-          initial={{ opacity: 0, scale: 0.5 }}
+        <span>
+          <motion.div
+            
+            initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-        >
-        </motion.span>
+            className="buttons"
+          >
+            <motion.a
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.95 }}
+            href="#Portfolio"
+            >
+              See my Latest Works
+            </motion.a>
+            {/* <Link> */}
+            <motion.a
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.95 }}
+              href="#Contact"
+            >
+              Contact Me
+            </motion.a>
+            {/* </Link> */}
+          </motion.div>
+          </span>
         <div className="social">
-          <a href="https://www.linkedin.com/in/aditya-mishra-36018918a/" target="_blank" rel="noopener noreferrer">
+          <motion.a
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            href="https://www.linkedin.com/in/aditya-mishra-36018918a/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src="/linkedin.png" alt="" />
-          </a>
-          <a href="https://github.com/Aditya060698" target="_blank" rel="noopener noreferrer">
+          </motion.a>
+          <motion.a
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5 }}
+            href="https://github.com/Aditya060698"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <img src="/github.png" alt="" />
-          </a>
+          </motion.a>
           {/* <a href="#">
             <img src="/youtube.png" alt="" />
           </a>
